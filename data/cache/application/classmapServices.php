82cc9e180c37a9b4d0839b3d7fe882ad4f4a5d98
Mahesh Mishra <web.technophilesdenn@gmail.com>
2025-10-27 00:32:18 +0530

    Initial commit: TPD CRM branding & setup

---
+++ data/cache/application/classmapServices.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'Email' => 'Espo\\Services\\Email',
+  'ExternalAccount' => 'Espo\\Services\\ExternalAccount',
+  'Import' => 'Espo\\Services\\Import',
+  'Integration' => 'Espo\\Services\\Integration',
+  'Record' => 'Espo\\Services\\Record',
+  'RecordTree' => 'Espo\\Services\\RecordTree',
+  'Stream' => 'Espo\\Services\\Stream',
+  'User' => 'Espo\\Services\\User'
+];
